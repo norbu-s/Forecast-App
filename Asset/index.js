eac7eff3d56d7def433dfe685c312174d2cba066
Norbu Sherpa <norbuts@hotmail.com>
2021-03-05 11:49:08 +1100

    Updated html page and Index.js with the first call

--- Asset/index.js
+++ Asset/index.js
@@ -1,1 +1,14 @@
-var cityList = $
+var cityList =$("#city-list");
+var cities = [];
+var key = "a4c4eca36ff9c16ac0c0487467c55f2c"
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+    $.ajax({
+        url: "http://www.openweathermap.org",
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        console.log(response.Runtime);
+      });
